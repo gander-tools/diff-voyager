fc61c08722ff82efe877415770638bb08b3ec6bf
Adam Gąsowski <gander@users.noreply.github.com>
2026-01-08 02:38:46 +0100

    style(frontend): reorder imports across project for consistency

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
